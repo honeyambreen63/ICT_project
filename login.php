2d7eb4d8711bdfef8da81cd51127769965b6c047
Ambreen Honey <honeyambreen63@gmail.com>
2025-12-19 10:37:18 +0500

    new additions of login in process

---
+++ login.php
@@ -0,0 +1,9 @@
+<form action="process.php" method="POST">
+  Email:
+  <input type="email" name="email" required>
+
+  Password:
+  <input type="password" name="password" required>
+
+  <button type="submit">Login</button>
+</form>
